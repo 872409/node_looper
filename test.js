0e9acb331219192db9cf68cb948d50ffaba43476
dawnxu <872409@gmail.com>
2012-06-20 16:09:19 +0800

    rename looper
    rename looper
    new test

--- test.js
+++ test.js
@@ -9,19 +9,19 @@ var stopWatch = require('./stopWatch.js').stopWatch;
 var sw = new stopWatch('process.nextTick Test');
  
 
-var _i = 0;
-function run(l,i){
-	var timeout = 1000+(Math.random()*1000);
-	setTimeout(
-		function(){
-			_i++;
-			console.log("index:"+i+" : runing:"+l.runing+"\tcomplated:"+_i + "\ttimeout:"+timeout);
-			l.callback(i);
-		},
-		timeout);
-};
+// var _i = 0;
+// function run(l,i){
+// 	var timeout = 1000+(Math.random()*1000);
+// 	setTimeout(
+// 		function(){
+// 			_i++;
+// 			console.log("index:"+i+" : runing:"+l.runing+"\tcomplated:"+_i + "\ttimeout:"+timeout);
+// 			l.callback(i);
+// 		},
+// 		timeout);
+// };
 // setTimeout 方法开始
-sw.start("setTimeout");
+// sw.start("setTimeout");
 // looper.create({name:"queue1",end:100,max:10},run,function(){
 // 	sw.stop();
 // 	// console.log(sw.getRecord());
@@ -34,8 +34,8 @@ sw.start("setTimeout");
 
 
 
-var getHtmlAction = function(index){
-		var _url  = "http://res.img.ifeng.com/2012/0620/wm_6d5a038f3b8eff4f4da642d27bf29c19.jpg";
+var getHtml = function(l,index){
+		var _url  = "http://www.baidu.com/";//"http://res.img.ifeng.com/2012/0620/wm_6d5a038f3b8eff4f4da642d27bf29c19.jpg";
 		var options =  url.parse(_url);
 		options.headers = {'cookie':"_"};
 		options.method = 'GET';
@@ -46,6 +46,7 @@ var getHtmlAction = function(index){
 			});
 			res.on('end', function() {
 				console.log("complate:"+index);
+				l.callback(index);
 			});
 		}).on('error', function(e) {
 		  console.log("Got error: " + e.message);
@@ -53,8 +54,14 @@ var getHtmlAction = function(index){
 		req.end();
 		// console.log("req:"+index);
 };
-for (var i = 0; i < 5000; i++) {
-	getHtmlAction(i);
-};
+
+// for (var i = 0; i < 5000; i++) {
+// 	getHtmlAction(i);
+// };
+sw.start("setTimeout");
+looper.each(0,5000,10,getHtml,function(){
+	sw.stop();
+	console.log(sw.getRecord());
+});
 
  
